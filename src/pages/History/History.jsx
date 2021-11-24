821d1a6a66361cfbdd906c682dc6610a3a7f515c
ilse31 <58392119+ilse31@users.noreply.github.com>
2021-11-24 17:38:23 +0700

    history added

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const History = () =>
 {
+    const [ histories, setHistories ] = useState( [] )
+
+    useEffect( () =>
+    {
+        let initial = localStorage.getItem( 'history' )
+        if ( initial )
+        {
+            initial = JSON.parse( initial )
+            setHistories( [ ...initial ] )
+        }
+    }, [] )
+
     return (
         <div className='container p-3'>
             <div className="row vh-100">
                 <div className="col">
                     <p>Chat History</p>
                     <p>List your recent chat history</p>
-                    <div className="card shadow text-black">
-                        <div className="card-body">
-                            <div className="row justify-content-between">
-                                <div className="col-sm-7">
-                                    <p>089530571642</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam nulla sit massa nunc  </p>
-                                </div>
-                                <div className="col-sm-3">
-                                    Wednesday Nov 12 2021, 0.48
+                    { histories.map( items => (
+                        <div className="card shadow text-black mb-2">
+                            <div className="card-body">
+                                <div className="row justify-content-between">
+                                    <div className="col-sm-7">
+                                        <p>{ items.number }</p>
+                                        <p>{ items.text }</p>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        { new Date( items.createdAt ).toDateString() }-{ new Date( items.createdAt ).toTimeString().substr( 0, 5 ) }
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    ) ) }
                 </div>
             </div>
         </div>
