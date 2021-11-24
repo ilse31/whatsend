5c8763cf0a7e16d57adf12af4ba7a6d18ff7be0d
ilse31 <58392119+ilse31@users.noreply.github.com>
2021-11-24 20:34:10 +0700

    add a something

--- src/pages/PhoneBook/PhoneBook.jsx
+++ src/pages/PhoneBook/PhoneBook.jsx
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
 import Contact from '../../components/Contact'
 import Modals from '../../components/Modals'
 
